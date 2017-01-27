009b80045526797d0e43f949d4b960745d9629f1
shibunath <shibunathshanker@ymail.com>
2017-01-27 20:55:40 +0530

    Fitbit Social Network - Spring MVC

---
+++ ECommerce Prototype/src/main/java/com/shikher/fitnetwork/trigger/Trigger.java
@@ -0,0 +1,26 @@
+package com.shikher.fitnetwork.trigger;
+
+/**
+ * Created by Shibu on 11/27/16.
+ */
+
+
+import javax.annotation.PostConstruct;
+import java.lang.*;
+
+public class Trigger {
+
+    @PostConstruct
+    public void Triggers() {
+
+        System.out.println("RUNNNING TRIGGERS");
+
+//        PreviousTriggers previousTriggers = new PreviousTriggers();
+//        previousTriggers.generatePreviousTriggers();
+
+//        TriggerSystem ts = new TriggerSystem();
+//        Thread t = new Thread(ts);
+//        t.start();
+    }
+
+}
